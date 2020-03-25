65f09ab98b2e26a4b6d130fd631823e54679ec67
petar <petar.baka@gmail.com>
2020-03-26 01:39:59 +0200

    map section numbers to addresses + streamline

--- izbori_mestni2019/tur2/ko/izbori_utils.py
+++ izbori_mestni2019/tur2/ko/izbori_utils.py
@@ -40,6 +40,7 @@ def get_filtered_results_by_station(filename, station_ids):
     extreme_votes = 0
     extreme_diff = 0
     extreme_stations = 0
+    extreme_ids = []
     for line in text:
         #station_id = int(line.split(';')[0])
         station_id = line.split(';')[0]
@@ -64,7 +65,9 @@ def get_filtered_results_by_station(filename, station_ids):
             maya.append(line[4]*100./(line[1] + line[4]))
             fnm.append((fanduk[-1], maya[-1], line[1] + line[4]))
 
-            if fanduk[-1]>70:  
+            if fanduk[-1]>80 and fnm[-1][-1]>100:
+            #if maya[-1]>80 and fnm[-1][-1]>100:
+                extreme_ids.append(station_id)
                 extreme_votes += line[1]
                 extreme_diff += line[1] - line[4]
                 extreme_stations +=1
@@ -83,15 +86,10 @@ def get_filtered_results_by_station(filename, station_ids):
 
     #x_pos = arange(section_ctr)    # equally spaced bars
     #width = .9       # the width of the bars: can also be len(x) sequence
+
     x_pos = [sum(section_votes[:i])+ 0.5*section_votes[i] for i in range(len(section_votes))]
     width = [0.95*x  for x in section_votes ]
     
-    p1 = plt.bar(x_pos, fanduk, width, color = 'b')
-    p2 = plt.bar(x_pos, maya, width, color = 'g',
-                 bottom=fanduk)
-    
-    plt.title('Резултати по секции (в проценти)'.decode('utf-8'), size = 20)
-    plt.legend((p1[0], p2[0]), ('Фандъкова'.decode('utf-8'), 'Манолова'.decode('utf-8')), loc = 'best', fontsize = 20)
 
     print "fanduk, maya"
     print totals[0], totals[1]
@@ -99,44 +97,48 @@ def get_filtered_results_by_station(filename, station_ids):
     print "extreme votes", extreme_votes
     print "extreme diff", extreme_diff
     print "extreme stations", extreme_stations
-    return results, maya, fanduk, x_pos, width
+    return results, maya, fanduk, x_pos, width, extreme_ids
 
 
+def generate_plots():
 
-station_ids =  load_station_ids('sections_03.11.2019.txt', 2246) # 2246 = Столична община (22 = код на район, 46 = код на община)
+    station_ids =  load_station_ids('sections_03.11.2019.txt', 2246) # 2246 = Столична община (22 = код на район, 46 = код на община)
+    results_by_station, maya, fanduk, x_pos, width, extreme_ids = get_filtered_results_by_station("votes_03.11.2019.txt", station_ids)
+    
+    fig1 = plt.figure(frameon = False) 
 
-fig1 = plt.figure(frameon = False) 
+    p1 = plt.bar(x_pos, fanduk, width, color = 'b')
+    p2 = plt.bar(x_pos, maya, width, color = 'g',
+                 bottom=fanduk)
+    
+    plt.title('Резултати по секции (в проценти)'.decode('utf-8'), size = 20)
+    plt.legend((p1[0], p2[0]), ('Фандъкова'.decode('utf-8'), 'Манолова'.decode('utf-8')), loc = 'best', fontsize = 20)
+    
+    plt.xticks(size = 15)
+    plt.yticks(size = 15)
+    #plt.xlim(0, 398718)
+    plt.ylim(0, 100)
+    plt.xlabel("Общо гласове".decode('utf-8'), size = 20)
+    
+    fig1.set_size_inches(10,6)
+    fig1.subplots_adjust(top = 0.9, bottom = 0.1, left = 0.07, right = 0.98)
+    plt.savefig("section_results.svg")
+    plt.savefig("section_results.png", dpi=1200)
+    #plt.close()
+   
 
-results_by_station, maya, fanduk, x_pos, width = get_filtered_results_by_station("votes_03.11.2019.txt", station_ids)
+def interactive_plot(): 
+    import plotly.graph_objects as go
 
-plt.xticks(size = 15)
-plt.yticks(size = 15)
-#plt.xlim(0, 398718)
-plt.ylim(0, 100)
-plt.xlabel("Общо гласове".decode('utf-8'), size = 20)
-
-fig1.set_size_inches(10,6)
-fig1.subplots_adjust(top = 0.9, bottom = 0.1, left = 0.07, right = 0.98)
-plt.savefig("section_results.svg")
-plt.savefig("section_results.png", dpi=1200)
-plt.close()
-
-#import plotly.graph_objects as go
-#M=len(x_pos)
-#animals= x_pos[:M]#['giraffes', 'orangutans', 'monkeys']
-#
-#fig = go.Figure(data=[
-#    go.Bar(name='Фандъкова'.decode('utf-8'), x=animals, width=width[:M], y=fanduk[:M]),
-#    go.Bar(name='Манолова'.decode('utf-8'), x=animals,  width=width[:M], y=maya[:M])
-#])
-## Change the bar mode
-#fig.update_layout(barmode='stack')
-#fig.write_html('section_results_bar_plot.html', auto_open=True)
-
-print "reading addresses"
-adresi = pd.read_excel('/Users/fun/Documents/skriptove_za_izobri/izbori_mestni2019/Sekcii-08.09.2019-KM-0.xlsx')
-print "addresses read" 
-
-print adresi.head()
-
-raw_input()
+    station_ids =  load_station_ids('sections_03.11.2019.txt', 2246) # 2246 = Столична община (22 = код на район, 46 = код на община)
+    results_by_station, maya, fanduk, x_pos, width, extreme_ids = get_filtered_results_by_station("votes_03.11.2019.txt", station_ids)
+    M=len(x_pos)
+    
+    fig = go.Figure(data=[
+        go.Bar(name='Фандъкова'.decode('utf-8'), x=x_pos[:M], width=width[:M], y=fanduk[:M]),
+        go.Bar(name='Манолова'.decode('utf-8'), x=x_pos[:M],  width=width[:M], y=maya[:M])
+    ])
+    # Change the bar mode
+    fig.update_layout(barmode='stack')
+    fig.write_html('section_results_bar_plot.html', auto_open=True)
+    
